ebba67a3f9968160479f0db6003e0b49578b629b
antony-guthiga <toniguthiga93@gmail.com>
2020-07-22 15:17:40 +0300

    import migration

--- manage.py
+++ manage.py
@@ -1,15 +1,17 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import Category
-
+from app.models import User, Category
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db, Category=Category )
+    return dict(app = app,db = db, User=User, Category=Category )
 if __name__ == '__main__':
     manager.run()
