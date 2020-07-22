0865217834c4897186b9e06fe346147c2f52ee5d
antony-guthiga <toniguthiga93@gmail.com>
2020-07-22 22:12:50 +0300

    add view function for new category

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 from flask_login import login_required
 from . import main
 from flask import render_template,url_for,request,redirect,abort
-from ..models import User
+from ..models import User,Category
 from .. import db
-from .forms import UpdateProfile
+from .forms import UpdateProfile,CategoryForm
 
 @main.route('/')
 def index():
@@ -16,7 +16,21 @@ def categories(id):
     if category is None:
         abort(404)
 
+    
     return render_template('category.html', category=category)
+
+@main.route('/add/category', methods=['GET', 'POST'])
+@login_required
+def new_category():
+    form = CategoryForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        new_category = Category(name = name)
+        new_category.save_category()
+
+        return redirect(url_for('.profile'))
+    title = 'New Category'
+    return render_template('new_category.html', category_form = form, title = title)
 
 @main.route('/user/<uname>')
 def profile(uname):
@@ -24,7 +38,6 @@ def profile(uname):
 
     if user is None:
         abort(404)
-
     return render_template("profile/profile.html", user = user)
 
 @main.route('/user/<uname>/update', methods=['GET', 'POST'])
