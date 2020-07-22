005544521fdae065a7d6d03ec19bb77d05344d1e
Makena-Maryann <maryann.makena00@gmail.com>
2020-07-22 21:55:57 +0300

    Create a form for creating notes

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,6 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
+class NoteForm(FlaskForm):
+    title = StringField('Pitch title',validators=[Required()])
+    pitch = TextAreaField('Pitch')
+    submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
