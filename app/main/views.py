c8c0f70c06a1c890ccad4fc4888262eda9b65bd0
Makena-Maryann <maryann.makena00@gmail.com>
2020-07-23 07:45:45 +0300

    Fixed bugs

--- app/main/views.py
+++ app/main/views.py
@@ -11,20 +11,24 @@ def index():
     title = 'Home - Welcome'
     return render_template('index.html', title=title)
 
-@main.route('/add',methods =['GET','POST'])
+@main.route('category/add-note/<int:id>',methods =['GET','POST'])
 @login_required
-def add_note():
+def add_note(id):
     form = NoteForm()
+    category = Category.query.filter_by(id=id).first 
+
+    if category is None:
+        abort(404)
 
     if form.validate_on_submit():
-        note = Note(title = form.title.data, note = form.pitch.data,user=current_user)
+        note = Note(title = form.title.data, note = form.pitch.data)
         
         db.session.add(note)
         db.session.commit()
 
-        return redirect(url_for('main.profile'))
+        return redirect(url_for('main.profile',id=category.id))
          
-    return render_template('add.html',note_form=form)
+    return render_template('add.html',note_form=form,category=category)
 
 
 @main.route('/categories/<int:id>')
@@ -32,9 +36,9 @@ def categories(id):
     category = Category.query.get(id)
     if category is None:
         abort(404)
-
     
-    return render_template('category.html', category=category)
+    notes = Note.query.order_by(Note.time.desc()).all()
+    return render_template('category.html', category=category,notes=notes)
 
 @main.route('/add/category', methods=['GET', 'POST'])
 @login_required
@@ -53,11 +57,12 @@ def new_category():
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
     notes = Note.query.order_by(Note.time.desc()).all()
+    categories = Category.get_categories()
 
     if user is None:
         abort(404)
 
-    return render_template("profile/profile.html", user = user,notes=notes)
+    return render_template("profile/profile.html", user = user,notes=notes,categories=categories)
 
 @main.route('/user/<uname>/update', methods=['GET', 'POST'])
 @login_required
